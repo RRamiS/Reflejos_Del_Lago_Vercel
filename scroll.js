dde5a4f643748e8b581b581e4b04a6a08acf2b76
Ramiro <105946615+RRamiS@users.noreply.github.com>
2023-08-18 18:48:29 -0300

    a
    s

---
+++ scroll.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector(".navbar");
+  
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 10) {
+        navbar.classList.add("navbar-scrolled");
+        
+      } else {
+        navbar.classList.remove("navbar-scrolled");
+        
+      }
+    });
+  });
+
+ 
