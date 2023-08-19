cb90448b68ee0a968e5b1a1e0185659f9905baac
Ramiro <105946615+RRamiS@users.noreply.github.com>
2023-08-19 10:54:58 -0300

    a
    a

--- scroll.js
+++ scroll.js
@@ -12,4 +12,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
- 
+  window.addEventListener('scroll', function () {
+    var nav = document.querySelector('nav');
+    var navLinks = document.querySelectorAll('.nav-link');
+    if (window.pageYOffset > 90) {
+      nav.classList.add('colorDropdown', 'shadow');
+      // Cambia el color de los enlaces del navbar a blanco
+      for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].style.color = 'white';
+      }
+    } else {
+      nav.classList.remove('colorDropdown', 'shadow');
+      // Restaura el color original de los enlaces del navbar
+      for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].style.color = ''; // Deja que se aplique el estilo del CSS
+      }
+    }
+  });
